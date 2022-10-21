a345e6b695e6fb0db7b98e586d7672610f047057
Istvan Viczian <viczian.istvan@gmail.com>
2022-10-21 10:11:09 +0200

    QR code generálás

--- hello-java/src/main/java/hello/HelloMain.java
+++ hello-java/src/main/java/hello/HelloMain.java
@@ -1,9 +1,14 @@
 package hello;
 
+import java.nio.file.Path;
+
 public class HelloMain {
 
     public static void main(String[] args) {
         var message = new HelloService().sayHello("John Doe");
         System.out.println(message);
+
+        // Paths.
+        new HelloService().writeCodeToFile("John Doe", Path.of("johndoe.png"));
     }
 }
--- hello-java/src/main/java/hello/HelloService.java
+++ hello-java/src/main/java/hello/HelloService.java
@@ -1,5 +1,13 @@
 package hello;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class HelloService {
 
     /**
@@ -10,5 +18,16 @@ public class HelloService {
      */
    public String sayHello(String name) {
        return "Hello " + name;
+   }
+
+   public void writeCodeToFile(String text, Path path) {
+       try {
+           var matrix = new MultiFormatWriter().encode(text, BarcodeFormat.QR_CODE, 200, 200);
+           MatrixToImageWriter.writeToPath(matrix, "png", path);
+       }
+       catch (WriterException | IOException e) {
+           throw new IllegalStateException("Can not create qr code", e);
+       }
+
    }
 }

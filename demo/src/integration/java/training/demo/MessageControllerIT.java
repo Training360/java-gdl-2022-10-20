e9ab624dab6302ad6ddb2a629414525fb489154d
Istvan Viczian <viczian.istvan@gmail.com>
2022-10-21 13:36:56 +0200

    DTO példányosítás paraméteres konstruktorral

--- demo/src/integration/java/training/demo/MessageControllerIT.java
+++ demo/src/integration/java/training/demo/MessageControllerIT.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MessageControllerIT {
 
@@ -17,6 +19,8 @@ class MessageControllerIT {
                 .get()
                 .uri("/")
                 .exchange()
-                .expectStatus().isOk();
+                .expectStatus().isOk()
+                .expectBody(Message.class).value(
+                        m -> assertThat(m.getText()).startsWith("Hello"));
     }
 }
--- demo/src/main/java/training/demo/Message.java
+++ demo/src/main/java/training/demo/Message.java
@@ -1,13 +1,18 @@
 package training.demo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class Message {
 
     private String text;
+
+    public Message(@JsonProperty("text") String text) {
+        this.text = text;
+    }
 }
